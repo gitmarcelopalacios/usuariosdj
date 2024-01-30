cad5c1fcb4397b9a1e606d0bfd54314260c89b8d
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2024-01-30 13:31:28 -0300

    completar registrar usuario

---
+++ usuarios/applications/users/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+
+from .models import User
+
+class UserRegisterForm(forms.ModelForm):
+    """Form definition for MODELNAME."""
+    password1 = forms. CharField(
+        label='Contraseña',
+        required=True, 
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+    
+    password2 = forms. CharField(
+        label='Contraseña',
+        required=True, 
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repetir Contraseña'
+            }
+        )
+    )
+
+    class Meta:
+        """Meta definition for UserRegisterform."""
+        
+
+        model = User
+        fields = (
+            'username',
+            'email',
+            'nombres',
+            'apellidos',
+            'genero',
+        )
--- usuarios/applications/users/views.py
+++ usuarios/applications/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import (
+    CreateView
+)
+
+from django.views.generic.edit import (
+    FormView
+)
+
+from .forms import UserRegisterForm
+
+class UserRegisterView(FormView):
+    template_name='users/register.html'
+    form_class = UserRegisterForm
+    success_url='/'
+    
+    
