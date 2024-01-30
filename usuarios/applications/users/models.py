2d2226988d4148d4e2dfedfd92af5df4d2ff9994
Marcelo Carlos Palacios <correodemarcelopalacios@gmail.com>
2024-01-30 11:42:56 -0300

    x

--- usuarios/applications/users/models.py
+++ usuarios/applications/users/models.py
@@ -1,29 +1,29 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-# Create your models here.
+from django.db import models
+from .managers import UserManager 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    
-    # el username debe ser UNICO
-    
     GENDER_CHOICES = (
-        ('M','MASCULINO'),
-        ('F','FEMENINO'),
-        ('O','OTROS'),
+        ('M', 'MASCULINO'),
+        ('F', 'FEMENINO'),
+        ('O', 'OTROS'),
     )
-    username = models.CharField(max_length=50, unique=True) 
+
+    username = models.CharField(max_length=10, unique=True) 
     email = models.EmailField()
-    nombres = models.CharField(max_length=30)
-    apellidos = models.CharField(max_length=30)
+    nombres = models.CharField(max_length=30, blank=True)
+    apellidos = models.CharField(max_length=30, blank=True)
     genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    is_staff = models.BooleanField(default=False)
+    
     
     USERNAME_FIELD = 'username'
-    
+    #EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager()
+
     def get_short_name(self):
         return self.username
     
     def get_full_name(self):
-        return self.nombres+' '+self.apellidos
-    
-    
-    
+        return f"{self.nombres} {self.apellidos}"
